69871a88af4544b362f90771ab3ce44b97a346dc
Bill Lorensen <bill.lorensen@gmail.com>
2014-01-26 11:48:22 -0500

    ENH: DisplayAllAdjacentModels is not configured.

--- Tools/DisplayAllAdjacencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-all=`ls -1 VTK| sort | cut -d":" -f2 | cut -d"." -f1,1`
-echo $all
-
-for i in $all; do
-/home/lorensen/ProjectsGIT/OpenAtlas/build/DisplayAdjacentModels $i /home/lorensen/ProjectsGIT/OpenAtlas/Data/SPLAtlas/hncma-atlas-lut.ctbl /home/lorensen/ProjectsGIT/OpenAtlas/Models/SPLAtlas/adjacencies.txt 
-done
