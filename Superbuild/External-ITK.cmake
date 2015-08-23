4f6b8c47f47035234ecc392bb193baaca005a85a
Bill Lorensen <lorensen@localhost.localdomain>
2015-08-23 15:23:04 -0400

    ENH: Bump ITK version

--- Superbuild/External-ITK.cmake
+++ Superbuild/External-ITK.cmake
@@ -1,7 +1,7 @@
 #---------------------------------------------------------------------------
 # Get and build itk
 
-set( ITK_TAG "v4.7.0" )
+set( ITK_TAG "v4.8.0" )
 
 ExternalProject_Add( ITK
   GIT_REPOSITORY "${git_protocol}://itk.org/ITK.git"
